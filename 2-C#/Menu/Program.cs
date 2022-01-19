0e1f1de38e74bc0a0afd7be0ab22af1e5972604b
Ganesh Avula <ganesh.avula123@gmail.com>
2022-01-19 14:42:06 -0600

    while

--- 2-C#/Menu/Program.cs
+++ 2-C#/Menu/Program.cs
@@ -2,31 +2,52 @@
 Console.WriteLine("Hello, World!");
 
     Console.Clear();
-    Console.WriteLine("Welcome to the Krusty Krab!");
-    Console.WriteLine("PLease tell me your name");
-    string name = Console.ReadLine();
-    Console.WriteLine($"Hello {name}, what would you like to order?");
-    Console.WriteLine("[1] - Krabby Patty");
-    Console.WriteLine("[2] - Exit");
+    bool repeat = true;
+  while (repeat)
+  {
+        Console.WriteLine("Welcome to the Krusty Krab!");
+        Console.WriteLine("PLease tell me your name");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hello {name}, what would you like to order?");
+        Console.WriteLine("[1] - Krabby Patty");
+        Console.WriteLine("[2] - Exit");
 
-    string userInput = Console.ReadLine();
+        string userInput = Console.ReadLine();
 
-    if(userInput == "1")
-    {
-        Console.WriteLine("Please choose your toppings");
-        Console.WriteLine("[1] - Ketchup");
-        Console.WriteLine("[2] - Mustard");
-        Console.WriteLine("[2] - Onions");
-        Console.WriteLine("[2] - Tomatoes");
-        Console.WriteLine("[2] - Lettuce");
+        if(userInput == "1")
+        {
+            Console.WriteLine("Please choose your toppings");
+            Console.WriteLine("[1] - Ketchup");
+            Console.WriteLine("[2] - Mustard");
+            Console.WriteLine("[3] - Ketchup & Mustard");
+            
 
-        string toppings = Console.ReadLine();
+            
+            string toppings = Console.ReadLine();
+            
 
-        Console.WriteLine($"did you want {toppings}");
+        if(toppings == "1")
+        {
+            Console.WriteLine("Enjoy your Krabby Patty with Ketchup!");
+
+        }
+        else if(toppings == "2")
+        {
+            Console.WriteLine("Enjoy your Krabby Patty with Mustard!");
+        }
+        else if(toppings == "3")
+        {
+            Console.WriteLine("Enjoy your Krabby Patty with Ketchup and Mustard!");
+        }
+            
+            
         
-        
-        int num1= Convert.ToInt32(Console.ReadLine());
-        int num2= Convert.ToInt32(Console.ReadLine());
+        }
 
-        Console.WriteLine($"The sum is: {num2+num1}");
-    }
+        if(userInput == "2")
+        {
+            Console.WriteLine("Have a nice day");
+        }
+}
+
+    
