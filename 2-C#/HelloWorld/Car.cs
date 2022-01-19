c10c9cb99ca879e30f42ad5844eb44b1627b0c68
Ganesh Avula <ganesh.avula123@gmail.com>
2022-01-19 11:50:06 -0600

    Car function

--- 2-C#/HelloWorld/Car.cs
+++ 2-C#/HelloWorld/Car.cs
@@ -2,11 +2,31 @@
 
 namespace CarFunction
 {
-//this is how you create a class by using the class keyword
-//public is just to make the class visible for everyon else
+    //this is how you create a class by using the class keyword
+    //public is just to make the class visible for everyon else
     
-}
+
 public class Car 
 {
+
+    //Field
+    //It is used to store info or define current state of the object when you first make it
+    // private means that only the classs itself can use that field
+    private string _color;
+    private string _owner;
+    private int _fuel;
+    private int _gallonPerMile;
+
+    //property
+    //they are in pascalcase
+
+    public string Color
+        {
+        get return _color;
+         }
+
+
+    }
+
 
 }
--- 2-C#/HelloWorld/Program.cs
+++ 2-C#/HelloWorld/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CarFunction;
+
+
 Console.WriteLine("Hello, World!");
+
+
+//NameOfClass NameofVariable = new NameOfClass();
+Car car1 = new Car();
