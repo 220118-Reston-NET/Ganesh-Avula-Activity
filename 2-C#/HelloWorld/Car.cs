1743d25343a46061390967a790073c48ada83838
Ganesh Avula <ganesh.avula123@gmail.com>
2022-01-19 11:31:02 -0600

    First Init

---
+++ 2-C#/HelloWorld/Car.cs
@@ -0,0 +1,12 @@
+//namespace is a great way to organize your code
+
+namespace CarFunction
+{
+//this is how you create a class by using the class keyword
+//public is just to make the class visible for everyon else
+    
+}
+public class Car 
+{
+
+}
